c8a30c643985f45efd8a50e91ff4c272dab573b2
akihito <ak.sahashi@gmail.com>
2026-07-08 10:52:57 -0700

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,6 +10,7 @@ defineProps({
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
+    <h2>TEST</h2>
     <h3>
       You’ve successfully created a project with
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
